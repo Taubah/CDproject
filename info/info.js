00866269ce9270bc6b3e681b7b1411430b7d1e35
Taubah <136009324+Taubah@users.noreply.github.com>
2023-07-16 05:10:39 +0000

    Edited border on all pages, edited top padding of title on pages, edited padding of tipsButton and gwButton, added information to info/gw page, and altered color of resources banner.

---
+++ info/info.js
@@ -0,0 +1,13 @@
+// var actionsList = $('.OA');
+// var add = $('.add');
+
+
+// add.on("click", addActionsToList);
+
+// function addActionsToList() {
+//   event.preventDefault();
+//   var store = $('.actions').val();
+// actionsList.append(`
+//     <p>${actions}</p>
+//   `);
+// }
